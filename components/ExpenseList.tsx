58ea6edd006fcefade0f6b415ba7fa2df9074b38
David Zarate <davidzarate@Davids-MacBook-Pro.local>
2024-12-01 20:18:55 -0600

    changes

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -31,39 +31,49 @@ export const ExpenseList = ({ search, handleLongPress }: IExpenseList) => {
     const formattedDate = `${months[month]} ${parseInt(day, 10)}, ${year}`;
     return formattedDate;
   };
+  const amount = globalData.reduce(
+    (acc, current): number => (acc += current.amount),
+    0
+  );
   return (
-    <FlatList
-      ListEmptyComponent={
-        <AnimatedGIF
-          url={require("../assets/empty.gif")}
-          title="Nothing to see."
-          unicode={EMOJIS.EMPTY}
-        />
-      }
-      data={globalData
-        ?.sort((a, b) => {
-          return b.date.localeCompare(a.date);
-        })
-        .filter((item) => item.date.includes(search))}
-      renderItem={({ item }) => (
-        <Pressable onLongPress={() => handleLongPress(item.photoUri)}>
-          <Card
-            containerStyle={{
-              flex: 1,
-              borderColor: "#a6ea7c",
-              borderRadius: 10,
-            }}
-          >
-            <Card.Title>{transformDate(item.date)}</Card.Title>
-            <Card.Divider />
-            <View style={{ display: "flex", alignItems: "center" }}>
-              <Text>Amount: ₡{item.amount}</Text>
-              <Text>Number of files: {item.photoUri.length}</Text>
-            </View>
-          </Card>
-        </Pressable>
-      )}
-      keyExtractor={(item) => item.id.toString()}
-    />
+    <View>
+      <View style={{ alignItems: "center" }}>
+        {" "}
+        <Text>Total: ₡{Number(amount).toLocaleString("es-CR")}</Text>
+      </View>
+      <FlatList
+        ListEmptyComponent={
+          <AnimatedGIF
+            url={require("../assets/empty.gif")}
+            title="Nothing to see."
+            unicode={EMOJIS.EMPTY}
+          />
+        }
+        data={globalData
+          ?.sort((a, b) => {
+            return b.date.localeCompare(a.date);
+          })
+          .filter((item) => item.date.includes(search))}
+        renderItem={({ item }) => (
+          <Pressable onLongPress={() => handleLongPress(item.photoUri)}>
+            <Card
+              containerStyle={{
+                flex: 1,
+                borderColor: "#a6ea7c",
+                borderRadius: 10,
+              }}
+            >
+              <Card.Title>{transformDate(item.date)}</Card.Title>
+              <Card.Divider />
+              <View style={{ display: "flex", alignItems: "center" }}>
+                <Text>Amount: ₡{item.amount}</Text>
+                <Text>Number of files: {item.photoUri.length}</Text>
+              </View>
+            </Card>
+          </Pressable>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
   );
 };
